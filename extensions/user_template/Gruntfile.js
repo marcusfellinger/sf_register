84ad54a069c86742677a0f5be2e8fe5f4417afa1
Marcus Fellinger <m.fellinger@signundsinn.de>
2022-04-11 22:03:58 +0200

    #12 Warning: Task "npmcopy:ckeditor" not found. -- Skip npmcopy:ckeditor for now

--- extensions/user_template/Gruntfile.js
+++ extensions/user_template/Gruntfile.js
@@ -25,7 +25,8 @@ module.exports = function (grunt) {
             compile_assets: ['scripts', 'css'],
             copy_static: ['copy:core_icons', 'copy:install_icons', 'copy:module_icons', 'copy:extension_icons', 'copy:fonts', 'copy:t3editor'],
             minify_assets: ['terser:thirdparty', 'terser:t3editor'],
-            npmcopy: ['npmcopy:ckeditor',
+            npmcopy: [
+//                'npmcopy:ckeditor',
 //                'npmcopy:ckeditor_externalplugins',
                 'npmcopy:dashboard',
 //                'npmcopy:umdToEs6',
