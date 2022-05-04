33d2ed1e8faf5965b7137c006f068fbaf75e58ad
DDEV-Local User <nobody@example.com>
2022-05-04 23:07:29 +0000

    Run composer update

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the TYPO3 CMS project.
  *
@@ -12,13 +13,8 @@
  * The TYPO3 project - inspiring people to share!
  */
 
-// Exit early if php requirement is not satisfied.
-if (PHP_VERSION_ID < 70200) {
-    die('This version of TYPO3 CMS requires PHP 7.2 or above');
-}
-
 // Set up the application for the frontend
-call_user_func(function () {
+call_user_func(static function () {
     $classLoader = require dirname(__DIR__).'/vendor/autoload.php';
     \TYPO3\CMS\Core\Core\SystemEnvironmentBuilder::run(0, \TYPO3\CMS\Core\Core\SystemEnvironmentBuilder::REQUESTTYPE_FE);
     \TYPO3\CMS\Core\Core\Bootstrap::init($classLoader)->get(\TYPO3\CMS\Frontend\Http\Application::class)->run();
