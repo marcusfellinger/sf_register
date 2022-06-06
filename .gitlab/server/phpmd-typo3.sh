29e530cac99009c1c89d095e7893dd4e6bb96df6
Marcus Fellinger <m.fellinger@signundsinn.de>
2022-06-06 23:16:30 +0200

    #12 Fix phpmd task

--- .gitlab/server/phpmd-typo3.sh
+++ .gitlab/server/phpmd-typo3.sh
@@ -3,7 +3,7 @@ mkdir -p "$(pwd)/.reports"
 
 [ -f ./.config/phpmd.xml ] && PHPMD_CONFIG=/app/.config/phpmd.xml || PHPMD_CONFIG=/app/.gitlab/phpmd.xml
 
-docker run -v "$(pwd)":/app 1drop/php-utils:$PHP_VERSION /composer/vendor/bin/phpmd \
+~/.composer/vendor/bin/phpmd \
   /app/extensions/ text "$PHPMD_CONFIG" \
   --reportfile=/app/.reports/phpmd-custom.txt \
   --exclude=*/vendor/* \
