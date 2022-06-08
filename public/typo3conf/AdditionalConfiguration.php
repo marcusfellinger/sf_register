2611273cfab1384eca1cd290a99fd7e6929d6808
Marcus Fellinger <m.fellinger@signundsinn.de>
2022-06-08 23:10:07 +0200

    #12 Show errors in Development context

--- public/typo3conf/AdditionalConfiguration.php
+++ public/typo3conf/AdditionalConfiguration.php
@@ -14,6 +14,11 @@ if ($applicationContext === 'Development') {
                         'user' => 'cg_grafing'
                     ]
                 ]
+            ],
+            'SYS'  => [
+                'trustedHostsPattern' => '.*.*',
+                'devIPmask'           => '*',
+                'displayErrors'       => 1,
             ]
         ]
     );
