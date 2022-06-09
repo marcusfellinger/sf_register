af76da66f97da03ba6cff455ce449b744462e147
Marcus Fellinger <m.fellinger@signundsinn.de>
2022-06-09 23:42:45 +0200

    #12 Fix phpmd messages

--- extensions/user_template/Classes/ContentObject/Exception/DebugExceptionHandler.php
+++ extensions/user_template/Classes/ContentObject/Exception/DebugExceptionHandler.php
@@ -5,9 +5,16 @@ namespace CGGrafing\Template\ContentObject\Exception;
 use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
 use TYPO3\CMS\Frontend\ContentObject\Exception\ExceptionHandlerInterface;
 
+/**
+ *
+ */
 class DebugExceptionHandler extends \TYPO3\CMS\Core\Error\DebugExceptionHandler implements ExceptionHandlerInterface
 {
 
+    /**
+     * @throws \Exception
+     * @inheritdoc
+     */
     public function handle(
         \Exception $exception,
         AbstractContentObject $contentObject = null,
