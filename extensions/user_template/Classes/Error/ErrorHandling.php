a765b943fdb3ab548392d1fce2ed41a657b8c083
Marcus Fellinger <m.fellinger@signundsinn.de>
2022-06-10 23:22:35 +0200

    #12 Wenn Link zu gesperrter Seite aufgerufen, wird Login-Seite anzeigen, und danach zur gesperrten Seite weiterleiten, falls Login erfolgreich.

---
+++ extensions/user_template/Classes/Error/ErrorHandling.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CGGrafing\Template\Error;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Error\PageErrorHandler\PageErrorHandlerInterface;
+use TYPO3\CMS\Core\Http\RedirectResponse;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ErrorHandling implements PageErrorHandlerInterface
+{
+
+    public function handlePageError(
+        ServerRequestInterface $request,
+        string $message,
+        array $reasons = []
+    ): ResponseInterface {
+        //check whether user is logged in
+        $context = GeneralUtility::makeInstance(Context::class);
+        if($context->getPropertyFromAspect('frontend.user', 'isLoggedIn')){
+            //show page with info that the access restricted page can't be visited because of missing access rights
+            return new RedirectResponse('/anmelden?return_url=' . $request->getUri()->getPath(), 403);
+        }
+        return new RedirectResponse('/anmelden?return_url=' . $request->getUri()->getPath(), 403);
+    }
+}
