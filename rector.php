49e9c0735af4a5f067f5c10413b457edc9f9a7a9
Marcus Fellinger <marcus@felliweb.de>
2023-03-08 23:09:49 +0100

    I-1 Run rector to upgrade to TYPO3 11

--- rector.php
+++ rector.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/extensions',
-        __DIR__ . '/public/typo3conf',
+        __DIR__ . '/extensions/user_template',
+        __DIR__ . '/public/typo3conf/AdditionalConfiguration.php',
+        __DIR__ . '/public/typo3conf/LocalConfiguration.php.php',
     ]);
 
     // register a single rule
@@ -17,6 +19,8 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80
+        LevelSetList::UP_TO_PHP_80,
+        Typo3SetList::TYPO3_11,
+        Typo3SetList::TCA_110
     ]);
 };
